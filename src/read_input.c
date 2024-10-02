5a5b3354b731c93c183be9901d181e0e8f2dc34e
Cosmin - Nicolae Stoia <cstoia@2-B-3.42heilbronn.de>
2024-10-02 12:02:42 +0200

    Created player.c, flood_fill.c and textures_colors.c in order to split the code for the norminette. Also added one more check for the map(Unknown texture identifier error)

--- src/read_input.c
+++ src/read_input.c
@@ -6,13 +6,14 @@
 /*   By: cstoia <cstoia@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 12:17:32 by cstoia            #+#    #+#             */
-/*   Updated: 2024/09/28 15:57:02 by cstoia           ###   ########.fr       */
+/*   Updated: 2024/10/02 11:50:06 by cstoia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
-static void	map_size(t_map *map, char *line)
+// Function to get the size of the map
+static void	ft_map_size(t_map *map, char *line)
 {
 	int	len;
 
@@ -22,6 +23,7 @@ static void	map_size(t_map *map, char *line)
 	if (len == 0)
 	{
 		printf("Error:\nInvalid map!");
+		// ft_cleanup();
 		exit(EXIT_FAILURE);
 	}
 	if (len > map->width)
@@ -29,105 +31,50 @@ static void	map_size(t_map *map, char *line)
 	map->height++;
 }
 
-static unsigned long	rgb_to_hex(int *rgb)
+// Function to expand a single line to the map's width by filling with spaces
+static void	ft_expand_line(char **line, int target_width)
 {
-	int	r;
-	int	g;
-	int	b;
-
-	r = rgb[0];
-	g = rgb[1];
-	b = rgb[2];
-	return ((r << 24) | (g << 16) | (b << 8) | 255);
-}
-
-static void	parse_textures_and_colors(t_map *map, char *line)
-{
-	char	**split_line;
-	char	**rgb_values;
-
-	split_line = ft_split(line, ' ');
-	if (!split_line[1])
-	{
-		printf("Error:\nNo space between texture/color identifiers and path/codes.\n");
-		//ft_cleanup();
-		exit(EXIT_FAILURE);
-	}
-	if (split_line[0] && !ft_strncmp(split_line[0], "NO", 2))
-		map->no = ft_strdup(ft_strtrim(split_line[1], " \n"));
-	else if (split_line[0] && !ft_strncmp(split_line[0], "SO", 2))
-		map->so = ft_strdup(ft_strtrim(split_line[1], " \n"));
-	else if (split_line[0] && !ft_strncmp(split_line[0], "WE", 2))
-		map->we = ft_strdup(ft_strtrim(split_line[1], " \n"));
-	else if (split_line[0] && !ft_strncmp(split_line[0], "EA", 2))
-		map->ea = ft_strdup(ft_strtrim(split_line[1], " \n"));
-	else if (split_line[0] && !ft_strncmp(split_line[0], "F", 1))
-	{
-		rgb_values = ft_split(split_line[1], ',');
-		map->floor[0] = ft_atoi(ft_strtrim(rgb_values[0], " \n"));
-		map->floor[1] = ft_atoi(ft_strtrim(rgb_values[1], " \n"));
-		map->floor[2] = ft_atoi(ft_strtrim(rgb_values[2], " \n"));
-		map->hex_flr = rgb_to_hex(map->floor);
-		// ft_cleanup(cub3d);
-	}
-	else if (split_line[0] && !ft_strncmp(split_line[0], "C", 1))
-	{
-		rgb_values = ft_split(split_line[1], ',');
-		map->ceiling[0] = ft_atoi(ft_strtrim(rgb_values[0], " \n"));
-		map->ceiling[1] = ft_atoi(ft_strtrim(rgb_values[1], " \n"));
-		map->ceiling[2] = ft_atoi(ft_strtrim(rgb_values[2], " \n"));
-		map->hex_clg = rgb_to_hex(map->ceiling);
-		// ft_cleanup(cub3d);
-	}
-	// ft_cleanup(cub3d);
-}
-
-static void	fill_empty_space(t_map *map)
-{
-	int		i;
-	int		j;
 	char	*new_line;
-	int		line_length;
+	int		j;
+
+	new_line = ft_calloc(target_width + 1, sizeof(char));
+	j = 0;
+	while ((*line)[j])
+	{
+		new_line[j] = (*line)[j];
+		j++;
+	}
+	while (j < target_width)
+	{
+		new_line[j] = ' ';
+		j++;
+	}
+	new_line[j] = '\0';
+	free(*line);
+	*line = new_line;
+}
+
+// Function to iterate through the map and expand lines where necessary
+static void	ft_fill_map_spaces(t_map *map)
+{
+	int	i;
+	int	line_length;
 
 	i = 0;
 	while (map->m_arr[i])
 	{
 		line_length = ft_strlen(map->m_arr[i]);
 		if (line_length < map->width)
-		{
-			new_line = ft_calloc(map->width + 1, sizeof(char));
-			j = 0;
-			while (map->m_arr[i][j])
-			{
-				new_line[j] = map->m_arr[i][j];
-				j++;
-			}
-			while (j < map->width)
-			{
-				new_line[j] = ' ';
-				j++;
-			}
-			new_line[j] = '\0';
-			free(map->m_arr[i]);
-			map->m_arr[i] = new_line;
-		}
+			ft_expand_line(&map->m_arr[i], map->width);
 		i++;
 	}
 }
 
-int	read_input(char *input, t_map *map)
+// Function to process each line and append map data or parse textures/colors
+static void	ft_process_line(int fd, t_map *map, char **concatenated_lines)
 {
-	int fd;
-	char *line;
-	char *concatenated_lines;
+	char	*line;
 
-	concatenated_lines = ft_calloc(1, 1);
-	fd = open(input, O_RDONLY);
-	if (fd == -1)
-	{
-		printf("Error:\nInvalid file");
-		exit(EXIT_FAILURE);
-	}
 	line = get_next_line(fd);
 	while (line != NULL)
 	{
@@ -138,18 +85,36 @@ int	read_input(char *input, t_map *map)
 			continue ;
 		}
 		if (ft_strncmp(line, "1", 1) != 0 && ft_strncmp(line, " ", 1) != 0)
-			parse_textures_and_colors(map, line);
+			ft_parse_textures_and_colors(map, line);
 		else
 		{
-			concatenated_lines = ft_strjoin_and_free(concatenated_lines, line);
-			map_size(map, line);
+			*concatenated_lines = ft_strjoin_and_free(*concatenated_lines,
+					line);
+			ft_map_size(map, line);
 		}
 		free(line);
 		line = get_next_line(fd);
 	}
+}
+
+// Main function to read input file, process lines, and finalize the map
+int	ft_read_input(char *input, t_map *map)
+{
+	int		fd;
+	char	*concatenated_lines;
+
+	concatenated_lines = ft_calloc(1, 1);
+	fd = open(input, O_RDONLY);
+	if (fd == -1)
+	{
+		printf("Error:\nInvalid file\n");
+		// ft_cleanup();
+		exit(EXIT_FAILURE);
+	}
+	ft_process_line(fd, map, &concatenated_lines);
 	map->m_arr = ft_split(concatenated_lines, '\n');
 	map->mapcopy = ft_split(concatenated_lines, '\n');
-	fill_empty_space(map);
+	ft_fill_map_spaces(map);
 	if (map->width > map->height)
 		map->scale = MAP_SIZE / map->width;
 	else
